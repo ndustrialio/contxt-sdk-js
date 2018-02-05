97d54a4d3993a8d1eb51fe9124ae5e06cdec7a4b
Greg Green <greg@ndustrial.io>
2018-02-05 17:05:41 -0500

    Update Request module to use "private" storage of axios and sdk

--- src/request.js
+++ src/request.js
@@ -2,46 +2,46 @@ import axios from 'axios';
 
 class Request {
   constructor(sdk) {
-    this.sdk = sdk;
-    this.axios = axios.create();
+    this._sdk = sdk;
+    this._axios = axios.create();
 
-    this.axios.interceptors.request.use(this._insertHeaders);
+    this._axios.interceptors.request.use(this._insertHeaders);
   }
 
   delete(...args) {
-    return this.axios.delete(...args);
+    return this._axios.delete(...args);
   }
 
   get(...args) {
-    return this.axios.get(...args);
+    return this._axios.get(...args);
   }
 
   head(...args) {
-    return this.axios.head(...args);
+    return this._axios.head(...args);
   }
 
   options(...args) {
-    return this.axios.options(...args);
+    return this._axios.options(...args);
   }
 
   patch(...args) {
-    return this.axios.patch(...args);
+    return this._axios.patch(...args);
   }
 
   post(...args) {
-    return this.axios.post(...args);
+    return this._axios.post(...args);
   }
 
   put(...args) {
-    return this.axios.put(...args);
+    return this._axios.put(...args);
   }
 
   request(...args) {
-    return this.axios.request(...args);
+    return this._axios.request(...args);
   }
 
   _insertHeaders = (config) => {
-    config.headers.common.Authorization = `Bearer ${this.sdk.auth.getCurrentApiToken()}`;
+    config.headers.common.Authorization = `Bearer ${this._sdk.auth.getCurrentApiToken()}`;
 
     return config;
   }
