1833b08b2add6501bc548e08b5137e04ef8987f8
Jonathan Gamble <jgamble@ndustrial.io>
2018-08-07 10:26:57 -0400

    updating a typedef label

--- src/utils/assets/formatAssetAttributeValueFromServer.js
+++ src/utils/assets/formatAssetAttributeValueFromServer.js
@@ -4,7 +4,7 @@
  * @param {Object} input
  * @param {string} input.asset_attribute_id UUID corresponding to the asset attribute
  * @param {string} input.asset_id UUID corresponding to the asset
- * @param {string} [input.asset_label] String label from the associated asset
+ * @param {string} [input.asset_label] Label from the associated asset
  * @param {string} input.created_at ISO 8601 Extended Format date/time string
  * @param {string} input.effective_date ISO 8601 Extended Format date/time string
  * @param {string} input.id UUID
