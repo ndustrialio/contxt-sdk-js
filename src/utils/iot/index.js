661f8b9de24c3d982793f89fd05226dfb7704856
Greg Green <greg@ndustrial.io>
2018-08-14 19:54:02 -0400

    Switch IOT module to use generic formatters

--- src/utils/iot/index.js
+++ src/utils/iot/index.js
@@ -1,9 +1,4 @@
-import formatOutputFieldFromServer from './formatOutputFieldFromServer';
 import formatOutputFieldDataFromServer from './formatOutputFieldDataFromServer';
 import parseOutputFieldNextPageUrlMetadata from './parseOutputFieldNextPageUrlMetadata';
 
-export {
-  formatOutputFieldFromServer,
-  formatOutputFieldDataFromServer,
-  parseOutputFieldNextPageUrlMetadata
-};
+export { formatOutputFieldDataFromServer, parseOutputFieldNextPageUrlMetadata };
