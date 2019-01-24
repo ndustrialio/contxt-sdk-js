1b876260d25a62bd8d0266de0fb649fbf2531f0a
leeallen337 <leeallen337@gmail.com>
2019-01-24 14:17:42 -0500

    Add login and logout function

--- src/sessionTypes/cliAuth.js
+++ src/sessionTypes/cliAuth.js
@@ -1,35 +1,125 @@
 import auth0 from 'auth0-js';
+import axios from 'axios';
+
+/**
+ * A SessionType that allows the user to initially authenticate with Auth0 and
+ * then gain a valid JWT from the Contxt Auth service. This would only
+ * be used in command line applications such as `contxt-cli`.
+ *
+ * @type SessionType
+ *
+ * @typicalname contxtSdk.auth
+ *
+ * @example
+ * const ContxtSdk = require('@ndustrial/contxt-sdk');
+ *
+ * const contxtService = new ContxtSdk({
+ *   config: {
+ *     auth: {
+ *       clientId: 'bleED0RUwb7CJ9j7D48tqSiSZRZn29AV'
+ *     }
+ *   },
+ *   sessionType: 'cliAuth'
+ * });
+ */
 
 class CliAuth {
+  /**
+   * @param {Object} sdk An instance of the SDK so the module can communicate with other modules
+   * @param {Object} sdk.audiences
+   * @param {Object} sdk.audiences.contxtAuth
+   * @param {string} sdk.audiences.contxtAuth.clientId The Auth0 client id of the
+   *   Contxt Auth environment
+   * @param {Object} sdk.config
+   * @param {Object} sdk.config.auth
+   * @param {string} sdk.config.auth.clientId The Auth0 client id of the application
+   */
   constructor(sdk) {
     this._sdk = sdk;
-
     this._sessionInfo = {};
 
     this._auth0 = new auth0.Authentication({
       domain: 'ndustrial.auth0.com',
       clientID: this._sdk.config.auth.clientId
     });
-    // this._auth0 = new auth0.WebAuth({
-    //   domain: 'ndustrial.auth0.com',
-    //   clientID: this._sdk.config.auth.clientId
-    // });
-
-    this._login();
   }
 
-  _login(config) {
-    return this._auth0.loginWithDefaultDirectory(
-      {
-        username: '',
-        password: ''
-      },
-      (err, response) => {
-        console.log('------');
-        console.log(JSON.stringify(err, null, 2));
-        console.log(response);
-      }
-    );
+  /**
+   * Logs the user in using Auth0 using a username a password
+   *
+   * @param {string} username The username of the user to authenticate
+   * @param {string} password The password of the user to authenticate
+   *
+   * @returns {Promise}
+   * @fulfills {string}
+   * @rejects {Error}
+   */
+  login(username, password) {
+    return new Promise((resolve, reject) => {
+      this._auth0.loginWithDefaultDirectory(
+        { password, username },
+        (err, response) => {
+          if (err) {
+            const errorMessage =
+              (err && err.description) || 'Authentication failed.';
+
+            return reject(new Error(errorMessage));
+          }
+
+          this._sessionInfo = {
+            grantInfo: response
+          };
+
+          return resolve('Authentication successful.');
+        }
+      );
+    });
+  }
+
+  /**
+   * Logs the user out by removing any stored session info.
+   */
+  logout() {
+    return new Promise((resolve) => {
+      this._sessionInfo = {};
+
+      return resolve('Logout successful - session info cleared.');
+    });
+  }
+
+  _getApiToken(accessToken) {
+    return axios
+      .post(
+        `${this._sdk.config.audiences.contxtAuth.host}/v1/token`,
+        {
+          audiences: Object.keys(this._sdk.config.audiences)
+            .map((audienceName) => {
+              return this._sdk.config.audiences[audienceName].clientId;
+            })
+            .filter((clientId) => {
+              return (
+                clientId &&
+                clientId !== this._sdk.config.audiences.contxtAuth.clientId
+              );
+            }),
+          nonce: 'nonce'
+        },
+        {
+          headers: { Authorization: `Bearer ${accessToken}` }
+        }
+      )
+      .then((data) => {
+        console.log('data');
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log('err');
+        console.log(err);
+      });
+  }
+
+  _saveSession(sessionInfo) {
+    this._sessionInfo = sessionInfo;
   }
 }
 
---
+++ src/sessionTypes/cliAuth.spec.js
@@ -0,0 +1,172 @@
+import auth0 from 'auth0-js';
+import CliAuth from './cliAuth';
+
+describe('sessionTypes/CliAuth', function() {
+  let authentication;
+  let authenticationSession;
+  let sdk;
+
+  beforeEach(function() {
+    this.sandbox = sandbox.create();
+  });
+
+  afterEach(function() {
+    this.sandbox.restore();
+  });
+
+  describe('constructor', function() {
+    let cliAuth;
+
+    beforeEach(function() {
+      sdk = {
+        config: {
+          audiences: {
+            contxtAuth: fixture.build('audience'),
+            facilities: fixture.build('audience')
+          },
+          auth: {
+            clientId: faker.internet.password()
+          }
+        }
+      };
+      authenticationSession = {
+        loginWithDefaultDirectory: this.sandbox.stub()
+      };
+      authentication = this.sandbox
+        .stub(auth0, 'Authentication')
+        .returns(authenticationSession);
+
+      cliAuth = new CliAuth(sdk);
+    });
+
+    it('appends the supplied sdk to the class instance', function() {
+      expect(cliAuth._sdk).to.deep.equal(sdk);
+    });
+
+    it('calls the auth0 Authentication constructor with the proper arguments', function() {
+      expect(authentication).to.be.calledOnce;
+      expect(authentication).to.be.calledWith({
+        domain: 'ndustrial.auth0.com',
+        clientID: cliAuth._sdk.config.auth.clientId
+      });
+    });
+
+    it('sets an initial, empty session info state', function() {
+      expect(cliAuth._sessionInfo).to.be.an('object');
+      expect(cliAuth._sessionInfo).to.be.empty;
+    });
+  });
+
+  describe('login', function() {
+    let cliAuth;
+
+    context('a successful login', function() {
+      let expectedResponse;
+      let password;
+      let promise;
+      let username;
+
+      beforeEach(function() {
+        sdk = {
+          config: {
+            audiences: {
+              contxtAuth: fixture.build('audience'),
+              facilities: fixture.build('audience')
+            },
+            auth: {
+              clientId: faker.internet.password()
+            }
+          }
+        };
+        password = faker.internet.password();
+        username = faker.internet.email();
+        expectedResponse = {
+          accessToken: faker.random.alphaNumeric(30),
+          idToken: faker.random.alphaNumeric(300),
+          scope: faker.lorem.sentence(),
+          expiresIn: faker.random.number({ min: 100, max: 1000 }),
+          tokenType: 'Bearer'
+        };
+
+        authenticationSession = {
+          loginWithDefaultDirectory: this.sandbox
+            .stub()
+            .yields(null, expectedResponse)
+        };
+        this.sandbox
+          .stub(auth0, 'Authentication')
+          .returns(authenticationSession);
+
+        cliAuth = new CliAuth(sdk);
+
+        promise = cliAuth.login(username, password);
+      });
+
+      it("calls the 'loginWithDefaultDirectory' function with the username and password", function() {
+        expect(
+          authenticationSession.loginWithDefaultDirectory
+        ).to.be.calledOnce;
+        expect(
+          authenticationSession.loginWithDefaultDirectory
+        ).to.be.calledWith({ username, password });
+      });
+
+      it('updates the session info', function() {
+        return promise.then(() => {
+          expect(cliAuth._sessionInfo).to.deep.equal({
+            grantInfo: expectedResponse
+          });
+        });
+      });
+
+      it('returns a fulfilled promise with a success message', function() {
+        return promise.then((response) => {
+          expect(response).to.equal('Authentication successful.');
+        });
+      });
+    });
+
+    context('a failed login', function() {
+      let expectedErrorMessage;
+      let password;
+      let promise;
+      let username;
+
+      beforeEach(function() {
+        sdk = {
+          config: {
+            audiences: {
+              contxtAuth: fixture.build('audience'),
+              facilities: fixture.build('audience')
+            },
+            auth: {
+              clientId: faker.internet.password()
+            }
+          }
+        };
+        password = faker.internet.password();
+        username = faker.internet.email();
+        expectedErrorMessage = faker.lorem.sentence();
+
+        authenticationSession = {
+          loginWithDefaultDirectory: this.sandbox
+            .stub()
+            .yields({ description: expectedErrorMessage })
+        };
+        this.sandbox
+          .stub(auth0, 'Authentication')
+          .returns(authenticationSession);
+
+        cliAuth = new CliAuth(sdk);
+
+        promise = cliAuth.login(username, password);
+      });
+
+      it('returns a rejected promise with an error message', function() {
+        return expect(promise).to.be.eventually.rejectedWith(
+          expectedErrorMessage
+        );
+      });
+    });
+  });
+});
