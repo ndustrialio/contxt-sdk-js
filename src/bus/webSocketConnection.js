9075e59c2e115a751c6cba880362d1a9f328ec4e
Eric <eric@ndustrial.io>
2019-01-31 11:10:48 -0500

    added functionalityto connect to the message bus via websocket. Created socket class to wrap the websocket so the user only has access to certain functionality with the websocket

---
+++ src/bus/webSocketConnection.js
@@ -0,0 +1,33 @@
+/**
+ * Module that wraps the websocket connection to the message bus
+ * to provide the developer with a specific set of functionality
+ *
+ */
+class WebSocketConnection {
+  /**
+   * @param {WebSocket} webSocket A WebSocket connection to the message bus
+   * @param {string} organizationId UUID corresponding with an organization
+   */
+  constructor(webSocket, organizationId) {
+    this._organizationId = organizationId;
+    this._webSocket = webSocket;
+  }
+
+  /**
+   * Closes the websocket connection
+   *
+   * @example
+   * contxtSdk.bus.connect('4f0e51c6-728b-4892-9863-6d002e61204d')
+   *   .then((webSocket) => {
+   *     webSocket.close()
+   *   })
+   *   .catch((errorEvent) => {
+   *     console.log(errorEvent);
+   *   });
+   */
+  close() {
+    this._webSocket.close();
+  }
+}
+
+export default WebSocketConnection;
