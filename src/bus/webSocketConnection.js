5bf40aa5416825177f95a3dedd60afadd26fd7db
Eric <eric@ndustrial.io>
2019-02-12 15:37:10 -0500

    added authorize functionality for a user to send a message to the message bus authorizing a channel for publishing and/or subscribing. Testing added

--- src/bus/webSocketConnection.js
+++ src/bus/webSocketConnection.js
@@ -9,8 +9,79 @@ class WebSocketConnection {
    * @param {string} organizationId UUID corresponding with an organization
    */
   constructor(webSocket, organizationId) {
+    this._jsonRpcId = 0;
     this._organizationId = organizationId;
     this._webSocket = webSocket;
+  }
+
+  /**
+   * Sends a message to the message bus to authorize a channel
+   *
+   * @param {string} token JSON Web Signature containing the channel and actions needed for authorization
+   *
+   * @returns {Promise}
+   * @fulfill {Object} Whether you are authorized or not and any error
+   * @reject {errorEvent} The error event from the WebSocket
+   *
+   * @example
+   * axios.post(['https://', SERVICE_ADDRESS, '/authorizations/channels'].join(''), {
+   *   resources: [
+   *     {
+   *       resource: 'CHANNEL_YOU_NEED',
+   *       actions: ['publish', 'subscribe']
+   *     }
+   *   ]
+   * }, {
+   *   headers: {
+   *     Authorization: ['Bearer', ACCESS_TOKEN_FROM_PREVIOUS_STEP].join(' ')
+   *   }
+   * }).then((response) => {
+   *   const token = response.data;
+   *
+   *   contxtSdk.bus.connect('4f0e51c6-728b-4892-9863-6d002e61204d')
+   *     .then((webSocket) => {
+   *       webSocket.authorize(token).then((res) => {
+   *         if(res.error) {
+   *          console.log("Error: ", res.error)
+   *         }
+   *
+   *         console.log(res.authorized)
+   *       })
+   *       .catch((authErrorEvent) => {
+   *         console.log(authErrorEvent)
+   *       });
+   *     })
+   *     .catch((errorEvent) => {
+   *       console.log(errorEvent);
+   *     });
+   * });
+   */
+  authorize(token) {
+    return new Promise((resolve, reject) => {
+      this._webSocket.onmessage = (message) => {
+        const messageData = JSON.parse(message.data);
+        const error = messageData.error || null;
+        const authorized = !error;
+
+        resolve({ authorized, error });
+      };
+
+      this._webSocket.onerror = (errorEvent) => {
+        reject(errorEvent);
+      };
+
+      this._webSocket.send(
+        JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'MessageBus.Authorize',
+          params: {
+            token
+          },
+          id: this._jsonRpcId
+        })
+      );
+      this._jsonRpcId++;
+    });
   }
 
   /**
--- src/bus/webSocketConnection.spec.js
+++ src/bus/webSocketConnection.spec.js
@@ -1,14 +1,16 @@
-import { WebSocket } from 'mock-socket';
-import WebScoketConnection from './webSocketConnection';
+import { Server, WebSocket } from 'mock-socket';
+import WebSocketConnection from './webSocketConnection';
 
-describe('WebScoketConnection', function() {
+describe.only('WebScoketConnection', function() {
   let expectedWebSocket;
+  let webSocketServer;
   let webSocketUrl;
 
   beforeEach(function() {
     this.sandbox = sandbox.create();
 
     webSocketUrl = `wss://${faker.internet.domainName()}`;
+    webSocketServer = new Server(webSocketUrl);
     expectedWebSocket = new WebSocket(webSocketUrl);
   });
 
@@ -16,6 +18,7 @@ describe('WebScoketConnection', function() {
     this.sandbox.restore();
 
     expectedWebSocket.close();
+    webSocketServer.close();
   });
 
   describe('constructor', function() {
@@ -24,7 +27,7 @@ describe('WebScoketConnection', function() {
 
     beforeEach(function() {
       expectedOrganization = fixture.build('organization');
-      ws = new WebScoketConnection(expectedWebSocket, expectedOrganization.id);
+      ws = new WebSocketConnection(expectedWebSocket, expectedOrganization.id);
     });
 
     it('sets a socket for the class instance', function() {
@@ -33,6 +36,198 @@ describe('WebScoketConnection', function() {
 
     it('sets an organization id for the class instance', function() {
       expect(ws._organizationId).to.equal(expectedOrganization.id);
+    });
+  });
+
+  describe('authorize', function() {
+    context('on a successful message', function() {
+      context('when a user is authorized', function() {
+        let expectedMessage;
+        let expectedOrganization;
+        let expectedJsonRpc;
+        let jsonRpcId;
+        let promise;
+        let send;
+        let token;
+        let ws;
+
+        beforeEach(function() {
+          expectedMessage = { jsonrpc: '2.0', id: '0', result: null };
+          expectedOrganization = fixture.build('organization');
+          send = this.sandbox.stub(expectedWebSocket, 'send');
+          token = faker.random.uuid();
+
+          ws = new WebSocketConnection(
+            expectedWebSocket,
+            expectedOrganization.id
+          );
+
+          jsonRpcId = ws._jsonRpcId;
+
+          expectedJsonRpc = JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'MessageBus.Authorize',
+            params: {
+              token
+            },
+            id: jsonRpcId
+          });
+
+          promise = ws.authorize(token);
+
+          webSocketServer.on('connection', (socket) => {
+            socket.send(JSON.stringify(expectedMessage));
+          });
+        });
+
+        it('sends a message to the message bus', function() {
+          return promise.then(() => {
+            expect(send).to.be.calledWith(expectedJsonRpc);
+          });
+        });
+
+        it('increments the jsonRpcId', function() {
+          return promise.then(() => {
+            expect(ws._jsonRpcId).to.equal(jsonRpcId + 1);
+          });
+        });
+
+        it('fulfills the promise', function() {
+          expect(promise).to.be.fulfilled;
+        });
+
+        it('recieves a message from the server', function() {
+          return promise.then((response) => {
+            expect(response.authorized).to.be.true;
+            expect(response.error).to.be.null;
+          });
+        });
+      });
+
+      context('when a user is not authorized', function() {
+        let expectedMessage;
+        let expectedOrganization;
+        let expectedJsonRpc;
+        let jsonRpcId;
+        let promise;
+        let send;
+        let token;
+        let ws;
+
+        beforeEach(function() {
+          expectedMessage = {
+            jsonrpc: '2.0',
+            id: '0',
+            error: 'user is not authorized'
+          };
+          expectedOrganization = fixture.build('organization');
+          send = this.sandbox.stub(expectedWebSocket, 'send');
+          token = faker.random.uuid();
+
+          ws = new WebSocketConnection(
+            expectedWebSocket,
+            expectedOrganization.id
+          );
+
+          jsonRpcId = ws._jsonRpcId;
+
+          expectedJsonRpc = JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'MessageBus.Authorize',
+            params: {
+              token
+            },
+            id: jsonRpcId
+          });
+
+          promise = ws.authorize(token);
+
+          webSocketServer.on('connection', (socket) => {
+            socket.send(JSON.stringify(expectedMessage));
+          });
+        });
+
+        it('sends a message to the message bus', function() {
+          return promise.then(() => {
+            expect(send).to.be.calledWith(expectedJsonRpc);
+          });
+        });
+
+        it('increments the jsonRpcId', function() {
+          return promise.then(() => {
+            expect(ws._jsonRpcId).to.equal(jsonRpcId + 1);
+          });
+        });
+
+        it('fulfills the promise', function() {
+          expect(promise).to.be.fulfilled;
+        });
+
+        it('recieves a message from the server', function() {
+          return promise.then((response) => {
+            expect(response.authorized).to.be.false;
+            expect(response.error).to.equal(expectedMessage.error);
+          });
+        });
+      });
+    });
+
+    context('on an unsuccessful message', function() {
+      let expectedOrganization;
+      let expectedJsonRpc;
+      let jsonRpcId;
+      let promise;
+      let send;
+      let token;
+      let ws;
+
+      beforeEach(function() {
+        expectedOrganization = fixture.build('organization');
+        send = this.sandbox.stub(expectedWebSocket, 'send');
+        token = faker.random.uuid();
+
+        ws = new WebSocketConnection(
+          expectedWebSocket,
+          expectedOrganization.id
+        );
+
+        jsonRpcId = ws._jsonRpcId;
+
+        expectedJsonRpc = JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'MessageBus.Authorize',
+          params: {
+            token
+          },
+          id: jsonRpcId
+        });
+
+        promise = ws.authorize(token);
+
+        webSocketServer.simulate('error');
+      });
+
+      it('sends a message to the message bus', function() {
+        return promise.catch(() => {
+          expect(send).to.be.calledWith(expectedJsonRpc);
+        });
+      });
+
+      it('increments the jsonRpcId', function() {
+        return promise.catch(() => {
+          expect(ws._jsonRpcId).to.equal(jsonRpcId + 1);
+        });
+      });
+
+      it('rejects the promise', function() {
+        expect(promise).to.be.rejected;
+      });
+
+      it('triggers an error event', function() {
+        return promise.catch((event) => {
+          expect(event.type).to.equal('error');
+        });
+      });
     });
   });
 
@@ -46,7 +241,7 @@ describe('WebScoketConnection', function() {
 
       close = this.sandbox.stub(expectedWebSocket, 'close');
 
-      ws = new WebScoketConnection(expectedWebSocket, expectedOrganization.id);
+      ws = new WebSocketConnection(expectedWebSocket, expectedOrganization.id);
 
       ws.close();
     });
