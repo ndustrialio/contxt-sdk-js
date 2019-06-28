596a82a910ee5e9d2492c9e00c8c8834fee24e2f
Greg Green <greg@ndustrial.io>
2019-06-28 13:10:43 -0400

    Remove unused chai-uuid package

--- support/addons.js
+++ support/addons.js
@@ -2,9 +2,7 @@
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const chaiUuid = require('chai-uuid');
 const sinonChai = require('sinon-chai');
 
 chai.use(chaiAsPromised);
-chai.use(chaiUuid);
 chai.use(sinonChai);
