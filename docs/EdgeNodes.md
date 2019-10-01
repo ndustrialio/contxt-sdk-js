6dbf4e753d0634d948b844041c4e8c62ea8a46d0
leeallen337 <leeallen337@gmail.com>
2019-10-01 11:02:24 -0400

    Update coordinator edge node submodule for legacy and tenant based urls

--- docs/EdgeNodes.md
+++ docs/EdgeNodes.md
@@ -6,25 +6,27 @@ Module that provides access to contxt edge nodes
 **Kind**: global class  
 
 * [EdgeNodes](#EdgeNodes)
-    * [new EdgeNodes(sdk, request, baseUrl)](#new_EdgeNodes_new)
-    * [.get(organizationId, edgeNodeClientId)](#EdgeNodes+get) ⇒ <code>Promise</code>
+    * [new EdgeNodes(sdk, request, baseUrl, [organizationId])](#new_EdgeNodes_new)
+    * [.get([organizationId], edgeNodeClientId)](#EdgeNodes+get) ⇒ <code>Promise</code>
 
 <a name="new_EdgeNodes_new"></a>
 
-### new EdgeNodes(sdk, request, baseUrl)
+### new EdgeNodes(sdk, request, baseUrl, [organizationId])
 
-| Param | Type | Description |
-| --- | --- | --- |
-| sdk | <code>Object</code> | An instance of the SDK so the module can communicate with other modules |
-| request | <code>Object</code> | An instance of the request module tied to this module's audience. |
-| baseUrl | <code>string</code> | The base URL provided by the parent module |
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| sdk | <code>Object</code> |  | An instance of the SDK so the module can communicate with other modules |
+| request | <code>Object</code> |  | An instance of the request module tied to this module's audience. |
+| baseUrl | <code>string</code> |  | The base URL provided by the parent module |
+| [organizationId] | <code>string</code> | <code>null</code> | The organization ID to be used in tenant url requests |
 
 <a name="EdgeNodes+get"></a>
 
-### contxtSdk.coordinator.edgeNodes.get(organizationId, edgeNodeClientId) ⇒ <code>Promise</code>
+### contxtSdk.coordinator.edgeNodes.get([organizationId], edgeNodeClientId) ⇒ <code>Promise</code>
 Get an edge node
 
-API Endpoint: '/organizations/:organizationId/edgenodes/:edgeNodeClientId'
+Legacy API Endpoint: '/organizations/:organizationId/edgenodes/:edgeNodeClientId'
+API Endpoint: 'edgenodes/:edgeNodeClientId'
 METHOD: GET
 
 **Kind**: instance method of [<code>EdgeNodes</code>](#EdgeNodes)  
@@ -33,7 +35,7 @@ METHOD: GET
 
 | Param | Type | Description |
 | --- | --- | --- |
-| organizationId | <code>string</code> | UUID |
+| [organizationId] | <code>string</code> | UUID Required when using the legacy API |
 | edgeNodeClientId | <code>string</code> |  |
 
 **Example**  
--- src/coordinator/edgeNodes.spec.js
+++ src/coordinator/edgeNodes.spec.js
@@ -28,96 +28,273 @@ describe('edgeNodes', function() {
   });
 
   describe('constructor', function() {
-    let edgeNodes;
+    context('when organization ID is provided', function() {
+      let edgeNodes;
+      let organizationId;
 
-    beforeEach(function() {
-      edgeNodes = new EdgeNodes(baseSdk, baseRequest, expectedHost);
+      beforeEach(function() {
+        organizationId = fixture.build('organization').id;
+
+        edgeNodes = new EdgeNodes(
+          baseSdk,
+          baseRequest,
+          expectedHost,
+          organizationId
+        );
+      });
+
+      it('sets a base url for the class instance', function() {
+        expect(edgeNodes._baseUrl).to.equal(expectedHost);
+      });
+
+      it('appends the supplied request module to the class instance', function() {
+        expect(edgeNodes._request).to.deep.equal(baseRequest);
+      });
+
+      it('appends the supplied sdk to the class instance', function() {
+        expect(edgeNodes._sdk).to.deep.equal(baseSdk);
+      });
+
+      it('sets the organization ID for the class instance', function() {
+        expect(edgeNodes._organizationId).to.equal(organizationId);
+      });
     });
 
-    it('sets a base url for the class instance', function() {
-      expect(edgeNodes._baseUrl).to.equal(expectedHost);
-    });
+    context('when organization ID is not provided', function() {
+      let edgeNodes;
 
-    it('appends the supplied request module to the class instance', function() {
-      expect(edgeNodes._request).to.deep.equal(baseRequest);
-    });
+      beforeEach(function() {
+        edgeNodes = new EdgeNodes(baseSdk, baseRequest, expectedHost);
+      });
 
-    it('appends the supplied sdk to the class instance', function() {
-      expect(edgeNodes._sdk).to.deep.equal(baseSdk);
+      it('sets a base url for the class instance', function() {
+        expect(edgeNodes._baseUrl).to.equal(expectedHost);
+      });
+
+      it('appends the supplied request module to the class instance', function() {
+        expect(edgeNodes._request).to.deep.equal(baseRequest);
+      });
+
+      it('appends the supplied sdk to the class instance', function() {
+        expect(edgeNodes._sdk).to.deep.equal(baseSdk);
+      });
+
+      it('sets the organization ID for the class instance', function() {
+        expect(edgeNodes._organizationId).to.equal(null);
+      });
     });
   });
 
   describe('get', function() {
-    context('all required params are provided', function() {
-      let edgeNodeFromServerAfterFormat;
-      let edgeNodeFromServerBeforeFormat;
-      let expectedEdgeNodeId;
-      let expectedOrganizationId;
-      let promise;
-      let request;
-      let toCamelCase;
+    context('legacy API', function() {
+      context('all required params are provided', function() {
+        let edgeNodeFromServerAfterFormat;
+        let edgeNodeFromServerBeforeFormat;
+        let expectedEdgeNodeId;
+        let expectedOrganizationId;
+        let promise;
+        let request;
+        let toCamelCase;
 
-      beforeEach(function() {
-        edgeNodeFromServerAfterFormat = fixture.build('edgeNode');
-        expectedEdgeNodeId = edgeNodeFromServerAfterFormat.id;
-        expectedOrganizationId = edgeNodeFromServerAfterFormat.organizationId;
-        edgeNodeFromServerBeforeFormat = fixture.build(
-          'edgeNode',
-          edgeNodeFromServerAfterFormat,
-          { fromServer: true }
-        );
+        beforeEach(function() {
+          edgeNodeFromServerAfterFormat = fixture.build('edgeNode');
+          expectedEdgeNodeId = edgeNodeFromServerAfterFormat.id;
+          expectedOrganizationId = edgeNodeFromServerAfterFormat.organizationId;
+          edgeNodeFromServerBeforeFormat = fixture.build(
+            'edgeNode',
+            edgeNodeFromServerAfterFormat,
+            { fromServer: true }
+          );
 
-        request = {
-          ...baseRequest,
-          get: sinon.stub().resolves(edgeNodeFromServerBeforeFormat)
-        };
-        toCamelCase = sinon
-          .stub(objectUtils, 'toCamelCase')
-          .returns(edgeNodeFromServerAfterFormat);
+          request = {
+            ...baseRequest,
+            get: sinon.stub().resolves(edgeNodeFromServerBeforeFormat)
+          };
+          toCamelCase = sinon
+            .stub(objectUtils, 'toCamelCase')
+            .returns(edgeNodeFromServerAfterFormat);
 
-        const edgeNodes = new EdgeNodes(baseSdk, request);
-        edgeNodes._baseUrl = expectedHost;
-        promise = edgeNodes.get(expectedOrganizationId, expectedEdgeNodeId);
-      });
+          const edgeNodes = new EdgeNodes(baseSdk, request);
+          edgeNodes._baseUrl = expectedHost;
+          promise = edgeNodes.get(expectedOrganizationId, expectedEdgeNodeId);
+        });
 
-      it('gets the edge node from the server', function() {
-        expect(request.get).to.be.calledWith(
-          `${expectedHost}/organizations/${expectedOrganizationId}/edgenodes/${expectedEdgeNodeId}`
-        );
-      });
+        it('gets the edge node from the server', function() {
+          expect(request.get).to.be.calledWith(
+            `${expectedHost}/organizations/${expectedOrganizationId}/edgenodes/${expectedEdgeNodeId}`
+          );
+        });
 
-      it('formats the edge node object', function() {
-        return promise.then(() => {
-          expect(toCamelCase).to.be.calledWith(edgeNodeFromServerBeforeFormat);
+        it('formats the edge node object', function() {
+          return promise.then(() => {
+            expect(toCamelCase).to.be.calledWith(
+              edgeNodeFromServerBeforeFormat
+            );
+          });
+        });
+
+        it('returns the requested edge node', function() {
+          return expect(promise).to.be.fulfilled.and.to.eventually.deep.equal(
+            edgeNodeFromServerAfterFormat
+          );
         });
       });
 
-      it('returns the requested edge node', function() {
-        return expect(promise).to.be.fulfilled.and.to.eventually.deep.equal(
-          edgeNodeFromServerAfterFormat
-        );
+      context('the organization ID is not provided', function() {
+        it('throws an error', function() {
+          const edgeNodes = new EdgeNodes(baseSdk, baseRequest);
+          const promise = edgeNodes.get();
+
+          return expect(promise).to.be.rejectedWith(
+            'An organizationId is required for getting an edge node.'
+          );
+        });
+      });
+
+      context('the edge node client ID is not provided', function() {
+        it('throws an error', function() {
+          const edgeNodes = new EdgeNodes(baseSdk, baseRequest);
+          const promise = edgeNodes.get('1');
+
+          return expect(promise).to.be.rejectedWith(
+            'An edgeNodeClientId is required for getting an edge node.'
+          );
+        });
       });
     });
 
-    context('the organization ID is not provided', function() {
-      it('throws an error', function() {
-        const edgeNodes = new EdgeNodes(baseSdk, baseRequest);
-        const promise = edgeNodes.get();
+    context('tenant API', function() {
+      context('all params are provided', function() {
+        let edgeNodeFromServerAfterFormat;
+        let edgeNodeFromServerBeforeFormat;
+        let expectedEdgeNodeId;
+        let expectedOrganizationId;
+        let promise;
+        let request;
+        let toCamelCase;
 
-        return expect(promise).to.be.rejectedWith(
-          'An organizationId is required for getting an edge node.'
-        );
+        beforeEach(function() {
+          edgeNodeFromServerAfterFormat = fixture.build('edgeNode');
+          expectedEdgeNodeId = edgeNodeFromServerAfterFormat.id;
+          expectedOrganizationId = edgeNodeFromServerAfterFormat.organizationId;
+          edgeNodeFromServerBeforeFormat = fixture.build(
+            'edgeNode',
+            edgeNodeFromServerAfterFormat,
+            { fromServer: true }
+          );
+
+          request = {
+            ...baseRequest,
+            get: sinon.stub().resolves(edgeNodeFromServerBeforeFormat)
+          };
+          toCamelCase = sinon
+            .stub(objectUtils, 'toCamelCase')
+            .returns(edgeNodeFromServerAfterFormat);
+
+          const edgeNodes = new EdgeNodes(
+            baseSdk,
+            request,
+            null,
+            expectedOrganizationId
+          );
+          edgeNodes._baseUrl = expectedHost;
+          promise = edgeNodes.get(expectedOrganizationId, expectedEdgeNodeId);
+        });
+
+        it('gets the edge node from the server and does not use the organization ID provided', function() {
+          expect(request.get).to.be.calledWith(
+            `${expectedHost}/edgenodes/${expectedEdgeNodeId}`
+          );
+        });
+
+        it('formats the edge node object', function() {
+          return promise.then(() => {
+            expect(toCamelCase).to.be.calledWith(
+              edgeNodeFromServerBeforeFormat
+            );
+          });
+        });
+
+        it('returns the requested edge node', function() {
+          return expect(promise).to.be.fulfilled.and.to.eventually.deep.equal(
+            edgeNodeFromServerAfterFormat
+          );
+        });
       });
-    });
 
-    context('the edge node client ID is not provided', function() {
-      it('throws an error', function() {
-        const edgeNodes = new EdgeNodes(baseSdk, baseRequest);
-        const promise = edgeNodes.get('1');
+      context('the organization ID is not provided', function() {
+        let edgeNodeFromServerAfterFormat;
+        let edgeNodeFromServerBeforeFormat;
+        let expectedEdgeNodeId;
+        let expectedOrganizationId;
+        let promise;
+        let request;
+        let toCamelCase;
 
-        return expect(promise).to.be.rejectedWith(
-          'An edgeNodeClientId is required for getting an edge node.'
-        );
+        beforeEach(function() {
+          edgeNodeFromServerAfterFormat = fixture.build('edgeNode');
+          expectedEdgeNodeId = edgeNodeFromServerAfterFormat.id;
+          expectedOrganizationId = edgeNodeFromServerAfterFormat.organizationId;
+          edgeNodeFromServerBeforeFormat = fixture.build(
+            'edgeNode',
+            edgeNodeFromServerAfterFormat,
+            { fromServer: true }
+          );
+
+          request = {
+            ...baseRequest,
+            get: sinon.stub().resolves(edgeNodeFromServerBeforeFormat)
+          };
+          toCamelCase = sinon
+            .stub(objectUtils, 'toCamelCase')
+            .returns(edgeNodeFromServerAfterFormat);
+
+          const edgeNodes = new EdgeNodes(
+            baseSdk,
+            request,
+            null,
+            expectedOrganizationId
+          );
+          edgeNodes._baseUrl = expectedHost;
+          promise = edgeNodes.get(expectedOrganizationId, expectedEdgeNodeId);
+        });
+
+        it('gets the edge node from the server', function() {
+          expect(request.get).to.be.calledWith(
+            `${expectedHost}/edgenodes/${expectedEdgeNodeId}`
+          );
+        });
+
+        it('formats the edge node object', function() {
+          return promise.then(() => {
+            expect(toCamelCase).to.be.calledWith(
+              edgeNodeFromServerBeforeFormat
+            );
+          });
+        });
+
+        it('returns the requested edge node', function() {
+          return expect(promise).to.be.fulfilled.and.to.eventually.deep.equal(
+            edgeNodeFromServerAfterFormat
+          );
+        });
+      });
+
+      context('the edge node client ID is not provided', function() {
+        it('throws an error', function() {
+          const organizationId = fixture.build('organization').id;
+          const edgeNodes = new EdgeNodes(
+            baseSdk,
+            baseRequest,
+            null,
+            organizationId
+          );
+          const promise = edgeNodes.get('1');
+
+          return expect(promise).to.be.rejectedWith(
+            'An edgeNodeClientId is required for getting an edge node.'
+          );
+        });
       });
     });
   });
